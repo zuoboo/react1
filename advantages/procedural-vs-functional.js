ad506c5f57e0c538844a18c2f8cf01f96a4c9231
saiki naoya <saikinaoya@saikinoMacBook-Pro.local>
2023-02-03 10:59:21 +0900

    関数型プログラミング　

---
+++ advantages/procedural-vs-functional.js
@@ -0,0 +1,20 @@
+// 手続き型プログラミング
+{
+    const octuples = [];
+    
+    for(let n = 1; n < 101; n += 1) {
+        if (n % 8 === 0) {
+            octuples.push(n);
+        }
+    }
+
+    console.log(octuples);
+}
+
+// 関数型プログラミング
+{
+    const range = (start,end) => [...Array(end - start)].map((_, n)=> n + start);
+    const octuples = range(1, 101).filter((n) => n % 8 === 0);
+    console.log(octuples);
+}
+
--- this/this-solutions.js
+++ this/this-solutions.js
@@ -6,7 +6,7 @@ class Person {
     // 1.関数にインスタンスのthisを束縛
     greet1() {
         function doIt() {
-            console.log(`Hi, I'm ${this.name}`);
+            console.log(`1) Hi, I'm ${this.name}`);
         }
         const boundDoIt = doIt.bind(this);
         boundDoIt();
@@ -20,5 +20,35 @@ class Person {
         doIt.call(this);
     }
 
+    // 3.変数_thisに値をうつしかえる。
+    greet3() {
+        const _this = this;
+        function doIt() {
+            console.log(`3) Hi, Im ${_this.name}`);
+        }
+        doIt()
+    }
 
+    // 4. アロー関数で定義する
+    greet4() {
+        const doIt = () => {
+            console.log(`4) Hi, I'm ${this.name}`);
+        };
+        doIt();
+    }
+
+    // 5.メソッド自身もアロー関数式で定義
+    greet5 = () => {
+        const doIt = () => {
+            console.log(`5) Hi, I'm ${this.name}`);
+        };
+        doIt();
+    }
 }
+
+const creamy = new Person('Mami');
+creamy.greet1();
+creamy.greet2();
+creamy.greet3();
+creamy.greet4();
+creamy.greet5();
